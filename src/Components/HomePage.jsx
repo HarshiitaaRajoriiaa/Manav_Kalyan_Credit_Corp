bd3623d7937686b7d75eba6e6698f5216388ef08
HarshiitaaRajoriiaa <harshitarajoria02@gmail.com>
2025-02-21 13:14:21 +0530

    update homepage

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -3,8 +3,7 @@ import Header from './Header'
 import Footer from './Footer'
 import AboutUs from './AboutUs'
 import Schemes from './Schemes'
-import Feedback from './Feedback'
-import ContactUs from './ContactUS'
+import ContactUs from './ContactUs'
 import LandingPage from './LandingPage'
 
 export default function HomePage() {
